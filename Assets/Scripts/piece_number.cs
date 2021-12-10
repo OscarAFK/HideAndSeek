eeadb2e55d97213c145d4e9f288084560c9509ea
hipGossoin <hippolyte.gossoin@gmail.com>
2021-12-11 00:22:17 +0100

    ui add

--- Assets/Scripts/piece_number.cs
+++ Assets/Scripts/piece_number.cs
@@ -5,39 +5,112 @@ using UnityEngine.UI;
 
 public class piece_number : MonoBehaviour
 {
-    [SerializeField] private Text text_piece;
-    [SerializeField] private Text max_piece;
+    [SerializeField] private Text text_piece1;
+    [SerializeField] private Text text_piece2;
+    [SerializeField] private Text text_piece3;
 
-    private int current_piece = 0;
-    private int current_max = 10;
+    private int current_piece1 = 0;
+    private int current_piece2 = 0;
+    private int current_piece3 = 0;
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown("a"))
+        {
+            current_piece1 += 1;
+            text_piece1.text = current_piece1.ToString();
+        }
         if (Input.GetKeyDown("q"))
         {
-            current_max += 1;
-            max_piece.text = current_max.ToString();
-        }
-        if (Input.GetKeyDown("s") && current_max > 0)
-        {
-            current_max -= 1;
-            if (current_max < current_piece)
+            if (current_piece1 < 5 && current_piece1 > 0)
             {
-                current_piece -= 1;
-                text_piece.text = current_piece.ToString();
+                current_piece1 = 0;
+                text_piece1.text = current_piece1.ToString();
             }
-            max_piece.text = current_max.ToString();
+            else
+            {
+                if (current_piece1 > 0)
+                {
+                    current_piece1 -= 5;
+                    text_piece1.text = current_piece1.ToString();
+                }
+            }
+            if (current_piece2 > 0)
+            {
+                current_piece2 -= 1;
+                text_piece2.text = current_piece2.ToString();
+            }
+            if (current_piece3 > 0)
+            {
+                current_piece3 -= 1;
+                text_piece3.text = current_piece3.ToString();
+            }
         }
-        if (Input.GetKeyDown("a")&& current_piece < int.Parse(max_piece.text))
+
+        if (Input.GetKeyDown("z"))
         {
-            current_piece += 1;
-            text_piece.text = current_piece.ToString();
+            current_piece2 += 1;
+            text_piece2.text = current_piece2.ToString();
         }
-        if (Input.GetKeyDown("z")&& current_piece>0)
+        if (Input.GetKeyDown("s"))
         {
-            current_piece -= 1;
-            text_piece.text = current_piece.ToString();
+            if (current_piece2 < 5 && current_piece2 > 0)
+            {
+                current_piece2 = 0;
+                text_piece2.text = current_piece2.ToString();
+            }
+            else
+            {
+                if (current_piece2 > 0)
+                {
+                    current_piece2 -= 5;
+                    text_piece2.text = current_piece2.ToString();
+                }
+                
+            }
+            if (current_piece1 > 0)
+            {
+                current_piece1 -= 1;
+                text_piece1.text = current_piece1.ToString();
+            }
+            if (current_piece3 > 0)
+            {
+                current_piece3 -= 1;
+                text_piece3.text = current_piece3.ToString();
+            }
+        }
+
+        if (Input.GetKeyDown("e"))
+        {
+            current_piece3 += 1;
+            text_piece3.text = current_piece3.ToString();
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            if (current_piece3 < 5 && current_piece3 > 0)
+            {
+                current_piece3 = 0;
+                text_piece3.text = current_piece3.ToString();
+            }
+            else
+            {
+                if (current_piece3 > 0)
+                {
+                    current_piece3 -= 5;
+                    text_piece3.text = current_piece3.ToString();
+                }
+            }
+            if (current_piece2 > 0)
+            {
+                current_piece2 -= 1;
+                text_piece2.text = current_piece2.ToString();
+            }
+            if (current_piece1 > 0)
+            {
+                current_piece1 -= 1;
+                text_piece1.text = current_piece1.ToString();
+            }
         }
     }
 }
